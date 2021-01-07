4bb7f6a32f9e252fa0221c862e7e1e384b6d9e5a
vivek-kaushik-245 <72548006+vivek-kaushik-245@users.noreply.github.com>
2021-01-07 10:26:21 +0530

    Create String_accept.c

---
+++ String_accept/String_accept.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{int i,n;
+char a[20];
+printf("NAME: SNIGDHA SRIVASTAVA");
+printf("\nCLASS: CSE 3D");
+printf("\nROLL NO: 1816110207");
+printf("\nEnter the term (to check whether it is in form of(ab*c(a+b)): ");
+scanf("%s",&a);
+n=strlen(a);
+if(a[0]=='a'&& (a[n-1]=='a'|| a[n-1]=='b') && a[n-2]=='c')
+{
+for(i=1;i<n-2;i++)
+{if(a[i]=='b')
+continue;
+else
+{printf("STRING IS REJECTED");
+exit(0);
+}
+}
+printf("STRING IS ACCEPTED");
+}
+else
+printf("STRING REJECTED");
+getch();
+}
